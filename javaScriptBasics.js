5cc632440405e5375d1440973b8042aba958cfc0
manas009 <manas.sahoo009@gmail.com>
2026-09-02 11:22:07 +0530

    javascript basics

---
+++ javaScriptBasics.js
@@ -0,0 +1,23 @@
+/* console.log("Hello, World!")
+
+console.log("step1")
+setTimeout(() => {console.log("step2")},3000)
+console.log("step3")
+
+var phoneNumber=9739984186
+console.log(phoneNumber)
+console.log(typeof phoneNumber) */
+
+
+let firstName="manas"
+console.log(typeof firstName)
+let companyName = "CZS"
+console.log(typeof companyName)
+let mobileNumber = 9739984186
+console.log(typeof mobileNumber)
+let isAutomation=true
+console.log(typeof isAutomation)
+let hasPlaywright=false
+console.log(typeof hasPlaywright)
+let automate
+console.log(typeof automate)
